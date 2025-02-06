e4d1a9987532f8a195bc7dba0ccbae145056d339
notcarlybates <carly30b@gmail.com>
2025-02-06 18:03:21 -0500

    logical guitar progress

--- guitar.py
+++ guitar.py
@@ -3,7 +3,17 @@ class String:
     def __init__(self, root_note, notes):
 
         self.root_note = root_note
-        self.all_notes = all_notes
+        self.notes = notes
+        self.index = index
+        self.string = string
+        
+    def make_string(root_note, notes, index):
+        root_index = notes.index(root_note)
+        string = []
+        for i in range(index):
+            note = notes[(root_index + i) % len(notes)]
+            string.append(note)
+            return string
 
 class Guitar:
 
@@ -11,21 +21,20 @@ class Guitar:
         self.tuning = tuning
         self.notes = notes
         self.frets = frets
+        self.fretboard = fretboard
 
     def make_tuning(self):
         print("Guitar created with "+self.tuning+" tuning")
+        
+    def assemble_neck(tuning, notes, index):
+        string_instance = String.make_string(tuning, notes, index)
+            
+    
 
 
 notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+tuning = ["E," "A", "D", "G", "B", "E"]
+root_note = "C#" # Will make input
+index = 25 # Defining the number of notes generated
 
-root_note = "C#"
-index = 27 # Defining the number of notes generated
-
-def make_string(root_note, notes, index):
-    root_index = notes.index(root_note)
-    for i in range(index):
-        note = notes[(root_index + i) % len(notes)]
-        print(i, note)
-            
-
-make_string(root_note, notes, index)
+Guitar.assemble_neck(tuning)
