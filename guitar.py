a531b094f68ab54e2c681b539e2f6ecf248b9b07
notcarlybates <carly30b@gmail.com>
2025-02-06 22:59:59 -0500

    chord type types differentiator

--- guitar.py
+++ guitar.py
@@ -1,40 +1,39 @@
 class String:
 
-    def __init__(self, root_note, notes):
+    def __init__(self, root_note, notes, frets):
 
         self.root_note = root_note
         self.notes = notes
-        self.index = index
-        self.string = string
+        self.frets = frets
+        self.string = []
         
-    def make_string(root_note, notes, index):
-        root_index = notes.index(root_note)
-        string = []
-        for i in range(index):
-            note = notes[(root_index + i) % len(notes)]
-            string.append(note)
-            return string
+    def make_string(self): # Creating an individual string based on a root note given in tuning
+        root_index = self.notes.index(self.root_note)
+        for i in range(self.frets):
+            note = self.notes[(root_index + i) % len(self.notes)]
+            self.string.append(note)
+        return self.string
+
 
 class Guitar:
 
-    def __init__(self, strings, notes, frets):
+    def __init__(self, tuning, notes, frets):
         self.tuning = tuning
         self.notes = notes
         self.frets = frets
-        self.fretboard = fretboard
-
-    def make_tuning(self):
-        print("Guitar created with "+self.tuning+" tuning")
+        self.fretboard = self.assemble_neck()
         
-    def assemble_neck(tuning, notes, index):
-        string_instance = String.make_string(tuning, notes, index)
-            
-    
+    def assemble_neck(self): # Put together individually created strings
+        return [String(root_note, self.notes, self.frets).make_string() for root_note in self.tuning]
+
+    def display_neck(self): # Display the compiled chords
+        for i, string in enumerate(self.fretboard):
+            print(f"{self.tuning[i]} : {string}")
 
 
 notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-tuning = ["E," "A", "D", "G", "B", "E"]
-root_note = "C#" # Will make input
-index = 25 # Defining the number of notes generated
+tuning = ["E", "A", "D", "G", "B", "E"]
+frets = 25 # Defining the number of notes generated (including root)
 
-Guitar.assemble_neck(tuning)
+guitar = Guitar(tuning, notes, frets)
+guitar.display_neck()
--- instance.py
+++
@@ -1,9 +0,0 @@
-from guitar import Guitar
-
-guitar1 = Guitar("EADGBE", ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"], 14) # Self is automatically passed with Python
-
-print(guitar1.notes)
-print(guitar1.tuning)
-print(guitar1.frets)
-
-guitar1.makeTuning()
